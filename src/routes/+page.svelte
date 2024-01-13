8fdbf497d47cb5172fccca9cff593ff315b89c59
Bethuel <bethropolis@gmail.com>
2024-01-13 23:31:17 +0000

    Refactor code to close splashscreen on DOMContentLoaded event

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
 <script>
-	import { invoke } from '@tauri-apps/api/tauri';
 	import Body from '../components/main/body.svelte';
-	import { onMount } from 'svelte';
-
-	onMount(async () => {
-		try {
-			await invoke('close_splashscreen');
-		} catch (e) {
-			// console.log(e);
-		}
-	});
 </script>
 
 <main class="overflow-x-hidden overflow-y-auto no-scrollbar w-full h-full">
