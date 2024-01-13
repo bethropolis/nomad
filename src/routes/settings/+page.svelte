b1de17f508402a471692e87562136f826dc7082a
Bethuel <bethropolis@gmail.com>
2024-01-13 06:40:14 +0300

    bug fix

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,32 @@
 <script>
+	import { onMount } from 'svelte';
 	import { Input } from 'stwui';
 	import { Toggle } from 'stwui';
 	import { settingsDB } from './../../db/table/setting.js';
 
-	export let data;
-Input
+	let settings = [];
+
+	onMount(async () => {
+		let all_settings = await settingsDB.getAllSettings();
+		settings = [
+			{
+				key: 'proxy',
+				value: all_settings.find((setting) => setting.key === 'proxy').value || '',
+				type: 'text'
+			},
+			{
+				key: 'repo',
+				value: all_settings.find((setting) => setting.key === 'repo').value || '',
+				type: 'text'
+			},
+			{
+				key: 'nsfw',
+				value: all_settings.find((setting) => setting.key === 'nsfw').value || false,
+				type: 'toggle'
+			}
+		];
+	});
+
 	function update(setting) {
 		settingsDB.updateSettings(setting.key, setting.value);
 	}
@@ -13,7 +35,7 @@ Input
 <main class="mx-8">
 	<h1>Settings</h1>
 	<div class="">
-		{#each data.settings as setting}
+		{#each settings as setting}
 			<div class="input-data my-2 w-3/4 flex">
 				<label class="w-3/12 text-lg" for={setting.key}>{setting.key}</label>
 				{#if setting.type === 'toggle'}
