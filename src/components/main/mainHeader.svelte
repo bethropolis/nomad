607d2181745d15d72be7ab01b8b51edb910cb81a
Bethuel <bethropolis@gmail.com>
2024-01-15 21:29:30 +0000

    Add search functionality and main header component

---
+++ src/components/main/mainHeader.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { searchQuery } from '../../store/store.js';
+  import { Input } from 'stwui';
+
+  let search = '';
+
+  function updateSearchQuery(event) { // Add event parameter here
+    if (event.key === 'Enter') { // Only update search when Enter is clicked
+      searchQuery.set(search);
+    }
+  }
+</script>
+
+<main class="flex justify-end w-full p-1 sticky top-0 bg-white dark:bg-gray-700 z-50">
+  <div class="search">
+    <Input type="text" placeholder="Search" class="min-w-[250px]" bind:value={search} on:keydown={updateSearchQuery} />
+  </div>
+</main>
--- src/components/main/showCard.svelte
+++ src/components/main/showCard.svelte
@@ -13,9 +13,16 @@
 </script>
 
 <div class="border dark:border-none border-gray-300 rounded-md overflow-hidden mx-1 flex-none shadow-md w-36 h-48 relative">
-        <img src={(proxy+cover)} alt="{title}"  class="object-cover object-center aspect-auto w-full h-full" />
+        <img src={(proxy+cover)} alt="{title}"  class="object-cover object-center opacity-90 aspect-auto w-full h-full" />
 </div>
 
 <style>
-    /* No specific styles needed here as TailwindCSS classes are used */
+   div:hover, div:focus, div:active {
+         transform: scale(1.05);
+         transition: transform 0.5s;
+   }
+   div:hover img, div:focus img, div:active img{
+            opacity: 1;
+            transition: opacity 0.5s;   
+   }
 </style>
