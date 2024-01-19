1b1093a3b69c77592e11d8a29372a42c37e22586
Bethuel <bethropolis@gmail.com>
2024-01-19 20:44:38 +0000

    Add reqwest dependency and implement make_request command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod req;
+
 use tauri::{Manager, Window};
 
 #[tauri::command]
-async fn close_splashscreen(window: Window) -> tauri::Result<String> {
-  window.get_window("splashscreen").expect("no window labeled 'splashscreen' found").close().unwrap();
-  window.get_window("main").expect("no window labeled 'main' found").show().unwrap();
-  
-  Ok("Splashscreen closed and main window opened".to_string())
+async fn make_request(url: String, options: req::CustomRequestOptions) -> Result<String, String> {
+    // Use the make_request_impl function from the request_handler module
+    req::make_request_impl(url, options).await
 }
- 
+
+#[tauri::command]
+async fn close_splashscreen(window: Window) -> tauri::Result<String> {
+    window
+        .get_window("splashscreen")
+        .expect("no window labeled 'splashscreen' found")
+        .close()
+        .unwrap();
+    window
+        .get_window("main")
+        .expect("no window labeled 'main' found")
+        .show()
+        .unwrap();
+
+    Ok("Splashscreen closed and main window opened".to_string())
+}
+
 fn main() {
     tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![close_splashscreen])
-    .run(tauri::generate_context!())
-    .expect("failed to run app");
+        .invoke_handler(tauri::generate_handler![close_splashscreen, make_request])
+        .run(tauri::generate_context!())
+        .expect("failed to run app");
 }
- 
--- src/lib/req.js
+++ src/lib/req.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { settingsDB } from '../db/db';
+import { invoke } from '@tauri-apps/api';
 
 /**
  * @typedef {Object} CustomHeaders
@@ -22,19 +23,22 @@ import { settingsDB } from '../db/db';
  *                   or rejects with an error if the request fails.
  */
 export async function request(url, options) {
-    try {
+	let proxy = await settingsDB.getSettings('proxy');
+	url = proxy?.value + url;
+    let response;
+	try {
+		if ('__TAURI__' in window) {
+			response = await invoke('make_request', { url, options });
+		} else {
+			response = await axios(url, options);
+		}
 
-        let proxy =await settingsDB.getSettings('proxy');
-        url = proxy?.value + url ; 
-        const response = await axios(url, options);
-        
-        if (response.status === 200) {
-            return response.data;
-        } else {
-            throw new Error('Failed to fetch manga list');
-        }
-    } catch (error) {
-        throw new Error('Failed to fetch manga list');
-    }
-
+		if (response.status === 200) {
+			return response.data;
+		} else {
+			throw new Error('Failed to fetch manga list');
+		}
+	} catch (error) {
+		throw new Error('Failed to fetch manga list');
+	}
 }
